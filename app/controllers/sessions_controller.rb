b3842307ff744e01440dc145cedb56edcfbf53e2
David Walden <email": null>
2016-06-25 18:11:41 -0400

    Change username to first name

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,7 @@ class SessionsController < ApplicationController
   end
 
   def create
-    @user = User.find_by(username: params['username'])
+    @user = User.find_by(email: params['email'])
     if @user && @user.authenticate(params['password'])
       session['user_id'] = @user.id
       redirect_to :root
