81e76a1fff3cbe079b0d740cd1197075c28fd9d4
George Skrapits <email": null>
2016-06-25 15:52:30 -0400

    tone_call works

--- app/models/reaction.rb
+++ app/models/reaction.rb
@@ -1,11 +1,10 @@
 class Reaction < ActiveRecord::Base
-  belongs_to :user
-  belongs_to :cstrength
-  has_many :answers
-  has_many :tones
-  has_many :questions, through: :answers, source: :question
-
   require_relative '../../lib/analysis.rb'
   include Analysis
 
+  belongs_to :user
+  belongs_to :cstrength
+  has_one :tone
+  has_many :answers
+  has_many :questions, through: :answers, source: :question
 end
--- lib/analysis.rb
+++ lib/analysis.rb
@@ -1,7 +1,31 @@
 module Analysis
+  def tone_call
+    text = ""
+    self.answers.each do |answer|
+      text += answer.body
+      text += " "
+    end
 
-  def tone_call(text)
-    `curl -u "33bd7472-b606-458e-9096-bf5654cae533":"7bkuxiZrMYBM" -H "Content-Type: application/json" -d "{\\"text\\": \\"#{text}\\"}" "https://gateway.watsonplatform.net/tone-analyzer/api/v3/tone?version=2016-05-19"`
+    response = `curl -u "33bd7472-b606-458e-9096-bf5654cae533":"7bkuxiZrMYBM" -H "Content-Type: application/json" -d "{\\"text\\": \\"#{text}\\"}" "https://gateway.watsonplatform.net/tone-analyzer/api/v3/tone?version=2016-05-19"`
+    response = JSON.parse(response)
+    binding.pry
+    Tone.create(
+      reaction_id: self.id,
+      user_id: self.user_id,
+      anger: response["document_tone"]["tone_categories"][0]["tones"][0]["score"],
+      disgust: response["document_tone"]["tone_categories"][0]["tones"][1]["score"],
+      fear: response["document_tone"]["tone_categories"][0]["tones"][2]["score"],
+      joy: response["document_tone"]["tone_categories"][0]["tones"][3]["score"],
+      sadness: response["document_tone"]["tone_categories"][0]["tones"][4]["score"],
+      analytical: response["document_tone"]["tone_categories"][1]["tones"][0]["score"],
+      confidence: response["document_tone"]["tone_categories"][1]["tones"][1]["score"],
+      tentative: response["document_tone"]["tone_categories"][1]["tones"][2]["score"],
+      openess: response["document_tone"]["tone_categories"][2]["tones"][0]["score"],
+      conscientiousness: response["document_tone"]["tone_categories"][2]["tones"][1]["score"],
+      extraversion: response["document_tone"]["tone_categories"][2]["tones"][2]["score"],
+      agreableness: response["document_tone"]["tone_categories"][2]["tones"][3]["score"],
+      emotional_range: response["document_tone"]["tone_categories"][2]["tones"][4]["score"]
+      )
   end
 
   def sentiment_call
@@ -11,8 +35,7 @@ module Analysis
       text += " "
     end
 
-    self.answers[0].body + " " + self.answers[1].body + " " + self.answers[2].body
     response = `curl -X POST --form "text=#{text}" --form "apikey=c35fab58-3208-4899-83ce-d61ab0b32218" "https://api.havenondemand.com/1/api/sync/analyzesentiment/v1"`
-    self.sentiment_rating = JSON.parse(response)['aggregate']['score']
+    self.update_attributes(sentiment_rating: JSON.parse(response)['aggregate']['score'])
   end
 end
