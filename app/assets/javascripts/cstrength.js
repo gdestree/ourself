06352d4c14ff6856da3bc463dc7f261d5f3e611f
George Skrapits <email": null>
2016-06-29 11:56:39 -0400

    style cstrength show page

--- app/assets/javascripts/cstrength.js
+++ app/assets/javascripts/cstrength.js
@@ -1,8 +1,10 @@
 $(document).ready(function(){
   $('.cstrength-container').on('mouseover', function(){
-    $(this).css("background-color", "rgba(255,255,255,1.0")
+    $(this).css("background-color", "rgba(255,255,255,1.0");
+    $(this).find('.tag-name').css("color", "black");
   });
   $('.cstrength-container').on('mouseleave', function(){
-    $(this).css("background-color", "transparent")
+    $(this).css("background-color", "transparent");
+    $(this).find('.tag-name').css("color", "white");
   })
 });
