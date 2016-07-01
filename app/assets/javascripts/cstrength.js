dce53ab6b19d2b9d4413b1bdf1ac57fee05f2f36
George Skrapits <email": null>
2016-07-01 09:56:05 -0400

    color fixes

--- app/assets/javascripts/cstrength.js
+++ app/assets/javascripts/cstrength.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+  $('.tag-name').css("color", "white");
   $('.cstrength-container').on('mouseover', function(){
     $(this).css("background-color", "rgba(255,255,255,1.0");
     $(this).find('.tag-name').css("color", "black");
