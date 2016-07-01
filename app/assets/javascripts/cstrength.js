b61ecd0c07e95b8324e92cdbf5965506b2f83dba
George Skrapits <email": null>
2016-07-01 10:09:28 -0400

    fix cstrength background color on mobile

--- app/assets/javascripts/cstrength.js
+++ app/assets/javascripts/cstrength.js
@@ -2,10 +2,12 @@ $(document).ready(function(){
   window.onpopstate = function() {
     $('.tag-name').css("color", "white");
     $('.links').css("color", "white");
+    $('.cstrength-container').css("background-color", "transparent");
   }
   $('.cstrength-container').on('mouseover', function(){
     $(this).css("background-color", "rgba(255,255,255,1.0");
     $(this).find('.tag-name').css("color", "black");
+    $(this).css("background-color", "white");
   });
   $('.cstrength-container').on('mouseleave', function(){
     $(this).css("background-color", "transparent");
